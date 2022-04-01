8cd6023bcbf5bc2af71b7ac0efcef34db988a843
Marko Nikolic <marko.supergun@gmail.com>
2022-04-01 14:32:12 +0200

    shell query files

--- src/functions.php
+++ src/functions.php
@@ -1,16 +1,24 @@
-<?php 
+<?php
 
 
-    function query($sql){
-        global $con;
-        
-        try{
-            $ex = mysqli_query($con,$sql);
-            if($ex){
-                return $ex;
-            }
-        } catch(Exception $e){
-
-        }
+function loged()
+{
+    if (isset($_SESSION['username'])) {
+        return true;
+    } else {
         return false;
     }
+}
+function query($sql)
+{
+    global $con;
+
+    try {
+        $ex = mysqli_query($con, $sql);
+        if ($ex) {
+            return $ex;
+        }
+    } catch (Exception $e) {
+    }
+    return false;
+}
--- src/index.php
+++ src/index.php
@@ -3,6 +3,10 @@
     require_once "./conf.php";
     include "./functions.php";
 
-    
+    if(loged()) {
+        header("Location: ./dashboard.php");
+    } else{
+        header("Location: ./login.php");
+    }
     
 ?>
