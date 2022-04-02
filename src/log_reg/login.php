fb5e676468e75720c39b0892d385c5e55d2aa612
Marko Nikolic <marko.supergun@gmail.com>
2022-04-02 22:45:15 +0200

    projekat se zove e studen't bato moj

--- src/log_reg/login.php
+++ src/log_reg/login.php
@@ -8,12 +8,12 @@
     <meta name="author" content="">
     <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
 
-    <title>Login form</title>
+    <title>E-Student</title>
 
     <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/sign-in/">
 
-    <!-- Bootstrap core CSS -->
-    <link href="../node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../node_modules/mdb-ui-kit/css/mdb.min.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
     <link href="./assets/css/style.css" rel="stylesheet">
@@ -22,18 +22,14 @@
 <body class="text-center">
     <form class="form-signin">
         <img class="mb-4" src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
-        <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
-        <label for="inputEmail" class="sr-only">Email address</label>
+        <h1 class="h3 mb-3 font-weight-normal">E-Student</h1>
+        <label for="inputEmail" class="sr-only">Indeks</label>
         <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-        <label for="inputPassword" class="sr-only">Password</label>
+        <label for="inputPassword" class="sr-only">Šifra</label>
         <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-        <div class="checkbox mb-3">
-            <label>
-                <input type="checkbox" value="remember-me"> Remember me
-            </label>
-        </div>
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-        <p class="mt-5 mb-3 text-muted">&copy; 2017-2018</p>
+
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Prijavi me</button>
+        <p class="mt-5 mb-3 text-muted">&copy; <?php echo date("Y"); ?></p>
     </form>
 </body>
 
